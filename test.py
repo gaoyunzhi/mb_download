42e0b33928612e0cc188439d2e53762f2965453e
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-09-05 18:19:30 -0700

    commit

--- test.py
+++ test.py
@@ -6,37 +6,23 @@ from telegram_util import compactText
 import os
 
 def findLinks(soup):
-	for x in soup.find_all('a', class_='sosad-button'):
-		yield x['href']
-
-def getSoup(url, force_cache = True, cookie = None):
-	return BeautifulSoup(cached_url.get(url, {'cookie': cookie}, 
-		force_cache = force_cache), 'html.parser')
-
-def getMainContent(link, cookie = None):
-	sub_soup = getSoup(link, force_cache = True, cookie = cookie)
-	return sub_soup.find('div', class_='main-text')
+	center = soup.find('div', id='list-chapterAll')
+	for item in center.find_all('a', href=True):
+		yield item['href']
 
 def getText(link):
-	main_content = getMainContent(link, cookie = None)
-	if not main_content:
-		os.system('rm %s' % cached_url.getFilePath(link))
-		main_content = getMainContent(link, cookie = cookie)
-	for att in ['font-4', 'text-left']:
-		item = main_content.find('div', class_=att)
-		if item:
-			item.decompose()
-	return main_content.get_text(separator='\n')
+	soup = BeautifulSoup(cached_url.get(link, encoding='gbk'), 'html.parser')
+	center = soup.find('div', class_='readcontent')
+	return center.text
 
-def download(url):
-	soup = BeautifulSoup(cached_url.get(url), 'html.parser')
+def download(main_url):
+	soup = BeautifulSoup(cached_url.get(main_url, encoding='gbk'), 'html.parser')
 	novel_name = soup.find('h1').text
-	print(novel_name.encode('utf-8').decode('gbk'))
-	# result = []
-	# for link in findLinks(soup):
-	# 	result.append(getText(link))
-	# with open('download/%s.txt' % novel_name, 'w') as f:
-	# 	f.write(compactText(''.join(result)))
+	result = []
+	for sub_url in findLinks(soup):
+		result.append(getText(main_url + sub_url))
+	with open('download/%s.txt' % novel_name, 'w') as f:
+		f.write(compactText(''.join(result)))
 	
 if __name__ == "__main__":
 	download('https://www.mbtxt.la/go/94213/')
